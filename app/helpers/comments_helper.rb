6ebacaefebfa52a85f5b1ab0f1740123b3fe0125
Devonte M <devontem@live.com>
2015-05-21 21:57:29 +0000

    updates firstname helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,6 +3,6 @@ module CommentsHelper
     def firstName(fullName)
         arr = []
         arr  = fullName.split(" ")
-        return arr[1]
+        return arr[0] + arr[1][0..1] + "."
     end
 end
