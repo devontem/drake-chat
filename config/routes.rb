56409d193fbbb935f9a6a6ecab4c0e30b01d881e
Devonte M <devontem@live.com>
2015-05-21 22:34:29 +0000

    added delete button to comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#auth_fail'
   get '/sign_out', to: 'sessions#destroy', as: :sign_out
-  get 'delete_comment', to 'comments'
   
 end
