d8a4ae3b80f4836d40d89228205513089599561c
Devonte M <devontem@live.com>
2015-05-22 18:54:34 +0000

    debug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,6 @@ class CommentsController < ApplicationController
        @comments = Comment.order('created_at DESC')
     end
     
-    def index
-        #/comments pages
-    #   redirect_to root_url 
-    end
-    
-    def show
-        #/comments/:id page
-        #@post = Post.find(params[:id])
-        redirect_to root_url
-    end
     
     def create
         respond_to do |format|
